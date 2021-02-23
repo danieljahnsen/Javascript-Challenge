0d06139327ddf5e8c8281e9ff5ab72575e739518
unknown <d.jahnsen.bes@gmail.com>
2021-02-23 00:18:21 -0500

    Table Data

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,13 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Using the code from Day 3 Activity 3
+var tbody = d3.select("tbody");
+
+data.forEach((data) => {
+    var row = tbody.append("tr");
+    Object.entries(data).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
