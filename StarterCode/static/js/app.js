43ecc66b7781e7c03d45e0056405c5c4a904365a
unknown <d.jahnsen.bes@gmail.com>
2021-03-01 17:50:06 -0500

    Final Commit

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -11,3 +11,46 @@ data.forEach((data) => {
       cell.text(value);
     });
   });
+
+// Button event code from day 3 activity 9
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+//Event handler function from day activty 9
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+
+  var filteredData = data.filter(event => event.datetime === inputValue);
+
+  console.log(filteredData);
+
+  //Remove existing tbody for new table
+  table1 = d3.select("tbody");
+  table1.html("");
+  
+  //Append the filtered data
+  filteredData.forEach((data) => {
+    var row = tbody.append("tr");
+    Object.entries(data).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+};
